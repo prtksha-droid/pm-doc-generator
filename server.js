65334c64167a29dce25b9627ebb42bf321b27d63
Pratiksha Bhattacharyya <prtksha@gmail.com>
2026-02-17 15:24:08 +0530

    Add web UI for PM Doc Generator

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ const app = express();
    EXPRESS SETUP
 ========================= */
 app.use(cors());
+app.use(express.static("public"));
+
 app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ extended: true }));
 
